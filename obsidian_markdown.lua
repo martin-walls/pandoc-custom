1ce0c9e96fbf4d1673a42304315370de0dda744e
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2022-01-24 20:26:32 +0000

    parse lists
    doesn't work properly when indenting with tabs, for some reason

--- obsidian_markdown.lua
+++ obsidian_markdown.lua
@@ -2,7 +2,9 @@
 local P, S, R, Cf, Cc, Ct, V, Cs, Cg, Cb, B, C, Cmt =
   lpeg.P, lpeg.S, lpeg.R, lpeg.Cf, lpeg.Cc, lpeg.Ct, lpeg.V,
   lpeg.Cs, lpeg.Cg, lpeg.Cb, lpeg.B, lpeg.C, lpeg.Cmt
-  --
+
+local tabsize = 2
+
 -- S matches any character in the string
 -- P matches string literally
 -- x + y matches either x or y
@@ -11,35 +13,46 @@ local P, S, R, Cf, Cc, Ct, V, Cs, Cg, Cb, B, C, Cmt =
 
 local whitespacechar = S(" \t\r\n")
 -- local specialchar = S("/*~[]\\{}|")
-local specialchar = S("[]|`*~=")
+local specialchar = S("[]|`*~=+-")
 local wordchar = (1 - (whitespacechar + specialchar))
 local spacechar = S(" \t")
 local newline = P"\r"^-1 * P"\n" -- at most one \r, followed by \n
 local blankline  = spacechar^0 * newline -- at least 0 spaces followed by a newline
-local endline = newline * #-blankline
-local cellsep = spacechar^0 * P"|"
+-- local endline = newline * #-blankline
+-- local cellsep = spacechar^0 * P"|"
+
+local bulletliststart = S("*+-")
+local orderedliststart = R"09" * P"." -- number followed by dot
 
 local function trim(s)
   return (s:gsub("^%s*(.-)%s*$", "%1"))
 end
 
--- local function ListItem(indent, startchars)
---   local start
---   if char == nul then
---     start = S"*#"
---   else
---     start = S(startchars)
---   end
---   local subitem = function(c)
---     return ListItem(indent + 2, c)
---   end
---   local parser = spacechar^indent
---                * start
---                * spacechar^1
---                * Ct((V"Inline" - (newline * spacechar^indent * S"*#"))^0)
---                * newline
---   return parser
--- end
+-- TODO make this work with tab indenting
+local function ListItem(level, start)
+  local spaceindent = level * tabsize
+  local indent = P" "^spaceindent + P"\t"^level
+  local subitem = function(s)
+    if level < 6 then
+      return ListItem(level + 1, s)
+    else
+      return (1 - 1) -- fails
+    end
+  end
+  local parser = indent
+               * start
+               * spacechar^1
+               * Ct((V"Inline" - (newline * indent * start))^0)
+               * newline
+               * (Ct(subitem(bulletliststart)^1) / pandoc.BulletList
+                  + Ct(subitem(orderedliststart)^1) / pandoc.OrderedList
+                  + Cc(nil))
+               / function (li, sublist)
+                   return { pandoc.Plain(li), sublist }
+                 end
+  return parser
+end
+
 
 -- GRAMMAR
 G = P{ "Doc",
@@ -56,7 +69,7 @@ G = P{ "Doc",
   Block = blankline^0
         * ( V"Header"
           + V"CodeBlock"
-          -- + V"List"
+          + V"List"
           + V"Para" ) ;
   -- paragraph
   Para = Ct(V"Inline"^1)
@@ -67,9 +80,12 @@ G = P{ "Doc",
          * spacechar^1
          * Ct((V"Inline"))
          / pandoc.Header ;
-  -- List = V"BulletList" ;
-  -- BulletList = Ct(ListItem(0,"-*")^1)
-  --            / pandoc.BulletList ;
+  List = V"BulletList"
+       + V"OrderedList";
+  BulletList = Ct(ListItem(0, bulletliststart)^1)
+             / pandoc.BulletList ;
+  OrderedList = Ct(ListItem(0, orderedliststart)^1)
+              / pandoc.OrderedList ;
   -- inline elements
   Inline = V"Emph"
          + V"Strong"
